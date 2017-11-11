2771e352800796b07fd4b83eeb99a209344aef27
Seong Yong-ju <sei40kr@gmail.com>
2017-11-11 11:16:00 +0900

    Added hello-world/formatted-print

---
+++ hello-world/formatted-print.rs
@@ -0,0 +1,11 @@
+fn main() {
+  println!("{} days", 31);
+  println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+  println!("{subject} {verb} {object}", object="the lazy dog", subject="the quick brown fox", verb="jumps over");
+
+  println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
+
+  println!("{number:>width$}", number=1, width=6);
+  println!("{number:>0width$}", number=1, width=6);
+}
