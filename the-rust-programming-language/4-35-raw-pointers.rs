ce4d179be4fad53885aae0ddde61ad346e6e4c2a
Seong Yong-ju <sei40kr@gmail.com>
2018-05-20 22:19:54 +0900

    Add raw-pointers

---
+++ the-rust-programming-language/4-35-raw-pointers.rs
@@ -0,0 +1,21 @@
+// 4-35-raw-pointers.rs
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+fn main() {
+    // safe
+    {
+        let x = 5;
+        let _raw = &x as *const i32;
+
+        let mut y = 10;
+        let _raw_mut = &mut y as *mut i32;
+    }
+
+    // dangerous
+    unsafe {
+        let x = 5;
+        let raw = &x as *const i32;
+
+        println!("raw points at {}", *raw);
+    }
+}
