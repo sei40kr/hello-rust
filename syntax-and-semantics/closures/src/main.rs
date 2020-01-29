0c52536b68810be75d04857f4347880251eb396d
Seong Yong-ju <sei40kr@gmail.com>
2020-01-29 22:51:39 +0900

    feat: add syntax-and-semantics/closures

---
+++ syntax-and-semantics/closures/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    {
+        let mut num = 5;
+        let mut add_num = |x: i32| num += x;
+        add_num(5);
+
+        assert_eq!(10, num);
+    }
+
+    {
+        let mut num = 5;
+        let mut add_num = move |x: i32| num += x;
+        add_num(5);
+
+        assert_eq!(5, num);
+    }
+}
