5fc4c1721d9d4a75f6a07b1314e896e44be5b95e
Seong Yong-ju <sei40kr@gmail.com>
2020-01-31 02:10:28 +0900

    feat: add effective-rust/concurrency

---
+++ effective-rust/concurrency/src/main.rs
@@ -0,0 +1,26 @@
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let data = Arc::new(Mutex::new(vec![1, 2, 3]));
+
+    let (tx, rx) = mpsc::channel();
+
+    for i in 0..3 {
+        let (data, tx) = (data.clone(), tx.clone());
+        thread::spawn(move || {
+            let mut data = data.lock().unwrap();
+            data[i] += 1;
+
+            tx.send(()).unwrap();
+        });
+    }
+
+    for _ in 0..10 {
+        rx.recv().unwrap();
+    }
+
+    thread::sleep(Duration::from_millis(50));
+}
