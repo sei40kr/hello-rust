39f1d34656d20162b55feed191dc0b3156fe8659
Seong Yong-ju <sei40kr@gmail.com>
2024-04-22 00:16:32 +0900

    add rust-atomics-and-locks/spinlock

---
+++ rust-atomics-and-locks/src/spinlock.rs
@@ -0,0 +1,51 @@
+use std::{
+    cell::UnsafeCell,
+    hint,
+    ops::{Deref, DerefMut},
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+impl<T> SpinLock<T> {
+    pub fn new(value: T) -> SpinLock<T> {
+        SpinLock {
+            locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn lock(&self) -> Guard<T> {
+        while self.locked.swap(true, Ordering::Acquire) {
+            hint::spin_loop();
+        }
+        Guard { lock: self }
+    }
+}
+
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.locked.store(false, Ordering::Release)
+    }
+}
