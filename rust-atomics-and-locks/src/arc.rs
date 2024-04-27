cffe79fc708a23134f22e3d3c09917b9a7accbe7
Seong Yong-ju <yongju.seong@codetakt.com>
2024-04-27 23:38:59 +0900

    add rust-atomics-and-locks/arc

---
+++ rust-atomics-and-locks/src/arc.rs
@@ -0,0 +1,89 @@
+use std::{
+    ops::Deref,
+    ptr::NonNull,
+    sync::atomic::{self, AtomicUsize, Ordering},
+};
+
+struct ArcData<T> {
+    ref_count: AtomicUsize,
+    data: T,
+}
+
+pub struct Arc<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+
+unsafe impl<T: Send + Sync> Send for Arc<T> {}
+unsafe impl<T: Send + Sync> Sync for Arc<T> {}
+
+impl<T> Arc<T> {
+    pub fn new(data: T) -> Arc<T> {
+        Arc {
+            ptr: NonNull::from(Box::leak(Box::new(ArcData {
+                ref_count: AtomicUsize::new(1),
+                data,
+            }))),
+        }
+    }
+
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+}
+
+impl<T> Deref for Arc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data().data
+    }
+}
+
+impl<T> Clone for Arc<T> {
+    fn clone(&self) -> Self {
+        if self.data().ref_count.fetch_add(1, Ordering::Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+
+        Arc { ptr: self.ptr }
+    }
+}
+
+impl<T> Drop for Arc<T> {
+    fn drop(&mut self) {
+        if self.data().ref_count.fetch_sub(1, Ordering::Release) == 1 {
+            atomic::fence(Ordering::Acquire);
+            unsafe { drop(Box::from_raw(self.ptr.as_ptr())) }
+        }
+    }
+}
+
+#[test]
+fn test() {
+    static NUM_DROPS: AtomicUsize = AtomicUsize::new(0);
+
+    struct DetectDrop;
+
+    impl Drop for DetectDrop {
+        fn drop(&mut self) {
+            NUM_DROPS.fetch_add(1, Ordering::Relaxed);
+        }
+    }
+
+    let x = Arc::new(("hello", DetectDrop));
+    let y = x.clone();
+
+    let t = std::thread::spawn(move || {
+        assert_eq!(x.0, "hello");
+    });
+
+    assert_eq!(y.0, "hello");
+
+    t.join().unwrap();
+
+    assert_eq!(NUM_DROPS.load(Ordering::Relaxed), 0);
+
+    drop(y);
+
+    assert_eq!(NUM_DROPS.load(Ordering::Relaxed), 1);
+}
